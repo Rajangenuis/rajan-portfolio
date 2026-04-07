6504d88c5060a06dba54bf1d109bed065d152201
Rajangenuis <rajan90genius@gmail.com>
2026-04-07 21:01:22 +0530

    Update typing effect speed and text content

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-const text = "Aspiring MIT student | CS + Electrical";
+// typing effect
+const text = "Aspiring IIT student | CS + Electrical";
 let i = 0;
 
 function typing() {
   if (i < text.length) {
     document.getElementById("typing").innerHTML += text.charAt(i);
     i++;
-    setTimeout(typing, 50);
+    setTimeout(typing, 40);
   }
 }
-
 typing();
+
+// cursor glow
 document.addEventListener("mousemove", (e) => {
   const cursor = document.querySelector(".cursor");
   cursor.style.left = e.clientX + "px";
   cursor.style.top = e.clientY + "px";
 });
+
+// project open
 function openProject() {
   window.location.href = "project1.html";
 }
+
+// scroll animation
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 300;
+    const height = sec.offsetHeight;
+
+    if (top > offset && top < offset + height) {
+      sec.classList.add("show");
+    }
+  });
+});
